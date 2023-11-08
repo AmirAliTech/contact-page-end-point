93739006af49b018ec02acf3bc8dba3e32f510fd
lazybluff <131444811+lazybluff@users.noreply.github.com>
2023-11-08 08:54:54 -0800

    Update App.js

--- contact-react-front-end/src/App.js
+++ contact-react-front-end/src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <form action='http://localhost:8000/contact' method='post'>
-        <input type='text' name='name' placeholder='enter your name'/>
-        <input type='email' name='email' placeholder='enter your email'/>
-        <input type='number'name='phone' placeholder='enter your phone'/>
-        <input type='text' name='address' placeholder='enter your adress'/>
-        <textarea cols='30' rows='10'name='message' placeholder='enter your message ...'/>
+        <input type='text' name='name' placeholder='enter your name' required/>
+        <input type='email' name='email' placeholder='enter your email' required/>
+        <input type='number'name='phone' placeholder='enter your phone' required/>
+        <input type='text' name='address' placeholder='enter your adress' required/>
+        <textarea cols='30' rows='10'name='message' placeholder='enter your message ...' required/>
         <input type='submit' value='send' className='btn'/>
         
       </form>
